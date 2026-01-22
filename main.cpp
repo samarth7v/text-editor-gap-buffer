840b399621d0cd7bd461ab2fa4fef00d09a7b3a0
samarth7v <samarth7v@gmail.com>
2026-01-22 17:39:42 +0530

    resolved enter key and other issues

--- main.cpp
+++ main.cpp
@@ -1,111 +1,126 @@
 #include <iostream>
-#include <string>
 #include <windows.h>
-#define MAX 100000
-#define MIN 100
-std::string s1(MAX,'\0');
-std::string s2(MAX,'\0');
-std::string buffer(MIN,'\0');
-int bufferpos=0;
-int s1pos=0;
-int s2pos=0;
-void passBuffer()
-{
-    int j=0;
-    while(s1pos<MAX && j<MIN){
-        s1[s1pos]=buffer[j];
-        s1pos++;j++;
+#include <string>
+struct gapBuffer{
+    char* data;
+    size_t start;
+    size_t end;
+    size_t capacity;
+}a;
+void expand(){
+    struct gapBuffer b;
+    b.capacity = 2*a.capacity;
+    b.data = new char[b.capacity];
+    b.start = a.start;
+    size_t k = (b.capacity - a.capacity);
+    b.end = a.end + k;
+    for(int i =0 ; i< a.start;i++){
+        b.data[i]=a.data[i];
     }
-    if(j<MIN) std::cout << "s1 exceeded0";
-    for(int i=0;i<MIN;i++){
-        buffer[i]='\0';
+    for(int i =b.end ; i< b.capacity;i++){
+        b.data[i]=a.data[i-k];
     }
-    bufferpos=0;
-}
-void addatcurrpos(char c){
-    if(bufferpos==MIN){
-        passBuffer();
-    }
-    buffer[bufferpos++] = c;
-}
-void print(){
-    system("cls");
-    if(s1pos>0)std::cout <<  s1;
-    // int i=0;
-    // while(i<s1pos){
-    //     std::cout << s1[i];
-    //     i++;
-    // }
-    // i=0;
-    // while(i<bufferpos){
-    //     std::cout << buffer[i];
-    //     i++;
-    // }
-    if(bufferpos>0)std::cout << buffer;
-    std::cout << '|';
-    // i=0;
-    // while(i<s2pos){
-    //     std::cout << s2[i];
-    //     i++;
-    // }
-    if(s2pos>0)std::cout << s2 ;
-    
-}
-// void read()
-// {
-//     char c = std::getchar();
-//     while(c != 'q')
-//     {
-//         addatcurrpos(c);
-//         print();
-//         c = std::cin.get();
-//     }
+    delete[] a.data;
+    a.data = b.data;
+    a.start = b.start;
+    a.end = b.end;
+    a.capacity = b.capacity;
 
+}
+void initialize(){
+    a.start=0;
+    a.end=2;
+    a.capacity = 2;
+    a.data=new char[a.capacity];
+}
+void write(char c){
+    if(a.start == a.end){
+        expand();
+    }
+    a.data[a.start++]=c;
+}
+// void clear_screen() {
+//     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+//     CONSOLE_SCREEN_BUFFER_INFO csbi;
+//     GetConsoleScreenBufferInfo(hOut, &csbi);
+
+//     DWORD cellCount = csbi.dwSize.X * csbi.dwSize.Y;
+//     DWORD written;
+
+//     COORD home = {0, 0};
+
+//     FillConsoleOutputCharacter(hOut, ' ', cellCount, home, &written);
+//     FillConsoleOutputAttribute(hOut, csbi.wAttributes, cellCount, home, &written);
+//     SetConsoleCursorPosition(hOut, home);
 // }
+
+COORD compute_cursor() {
+    COORD c{0, 0};
+    for (size_t i = 0; i < a.start; ++i) {
+        if (a.data[i] == '\n') {
+            ++c.Y;
+            c.X = 0;
+        } else {
+            ++c.X;
+        }
+    }
+    return c;
+}
+void print() {
+    std::cout << "\x1b[H";
+    std::cout << "\x1b[J";
+
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD home = {0,0};
+    SetConsoleCursorPosition(hOut, home);
+    SHORT x = 0, y = 0;
+    std::string buffer;
+    for (size_t i = 0; i < a.start; ++i) {
+        if (a.data[i] == '\n') {
+            buffer += "\x1b[K\n";
+            ++y;
+            x = 0;
+        } else {
+            buffer += a.data[i];
+            ++x;
+        }
+    }
+
+    for (size_t i = a.end; i < a.capacity; ++i) {
+        if (a.data[i] == '\n') {
+            buffer += "\x1b[K\n";
+        } else {
+            buffer += a.data[i];
+        }
+    }
+    buffer+=" ";
+    std::cout << buffer;
+    COORD cur;
+    cur.X = x;
+    cur.Y = y;
+    SetConsoleCursorPosition(hOut, cur);
+}
+
 void left(){
-    char k;
-    if(bufferpos>0){
-        k = buffer[--bufferpos];
-        buffer[bufferpos]='\0';
-    }
-    else if(s1pos == 0)return;
-    else {
-        k=s1[--s1pos];
-        s1[s1pos]='\0';
-    }
-    std::string s3(MAX,'\0');
-    s3[0] = k;
-    int i=1,j=0;
-    while(i<MAX && j<MAX && j<s2pos)
-    {
-        s3[i]=s2[j];
-        i++;j++;
-    }
-    s2 = s3;
-    s2pos++;
+    if(a.start==0) return;
+    a.data[--a.end] = a.data[--a.start];
 }
 void right(){
-    char k;
-    if(s2pos==0) return;
-    if(bufferpos<MIN){
-        buffer[bufferpos++]=s2[0];
-    }
-    else {
-        passBuffer();
-        buffer[bufferpos++]=s2[0];
-    }
-    std::string s3(MAX,'\0');
-    int i=0,j=1;
-    while(i<MAX && j<MAX && j<s2pos)
-    {
-        s3[i]=s2[j];
-        i++;j++;
-    }
-    s2 = s3;
-    s2pos--;
-    if(bufferpos==MIN){
-        passBuffer();
-    }
+    if(a.end==a.capacity)return;
+    a.data[a.start++] = a.data[a.end++];
+}
+void escape(){
+    exit(0);
+}
+void back(){
+    if(a.start==0)return;
+    a.start--;
+    print();
+}
+void enter(){
+    write('\n');
+    print();
 }
 void mode()
 {
@@ -114,32 +129,41 @@ void mode()
     GetConsoleMode(hIn,&mode);
     mode &= ~ (ENABLE_LINE_INPUT) ;
     mode &= ~ (ENABLE_ECHO_INPUT) ;
-    mode &= ~ (ENABLE_VIRTUAL_TERMINAL_INPUT) ;
+    // mode &= ~ (ENABLE_VIRTUAL_TERMINAL_INPUT) ;
 
     SetConsoleMode(hIn,mode);
     INPUT_RECORD rec;
     DWORD read;
+    HANDLE hOutt = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD modet;
+    GetConsoleMode(hOutt, &modet);
+    modet |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    modet |= ENABLE_PROCESSED_OUTPUT;
+    SetConsoleMode(hOutt, modet);
     while (true) {
         ReadConsoleInput(hIn, &rec, 1, &read);
         if (rec.EventType == KEY_EVENT) {
             auto& k = rec.Event.KeyEvent;
 
             if (k.bKeyDown) {
-                if (k.uChar.AsciiChar) {
-                    char c = rec.Event.KeyEvent.uChar.AsciiChar;
-                    if (c == 'q') break;
-                    if (c != 0){
-                        addatcurrpos(c);
-                        print();
-                    }
-                } else {
-                    switch (k.wVirtualKeyCode) {
-                        case VK_LEFT: left();print();break;
-                        case VK_RIGHT:right();print();break;
-                        case VK_UP:
-                        case VK_DOWN:
-                            break;
-                    }
+                bool isSpecial = true;
+            
+                switch (k.wVirtualKeyCode) {
+                    case VK_LEFT:   left(); print(); break;
+                    case VK_RIGHT:  right(); print(); break;
+                    case VK_ESCAPE: escape(); break;
+                    case VK_RETURN: enter(); break; 
+                    case VK_BACK:   back(); break;  
+                    case VK_UP:     break;
+                    case VK_DOWN:   break;
+                    default:        isSpecial = false; break;
+                }
+
+                if (!isSpecial && k.uChar.AsciiChar >= 32) {
+                    if (k.uChar.AsciiChar == 'q') break; // Quit option
+                    
+                    write(k.uChar.AsciiChar);
+                    print();
                 }
             }
         }
@@ -152,10 +176,6 @@ void mode()
     SetConsoleMode(hIn,mode2);
 }
 int main(){
-    s1[0]='h';
-    s1[1]='o';
-    s1[2]='o';
-    bufferpos = 0;
-    s1pos = 3;
+    initialize();
     mode();
 }
